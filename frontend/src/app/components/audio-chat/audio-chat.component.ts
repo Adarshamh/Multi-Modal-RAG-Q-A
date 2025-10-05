a3ee68589adafb2f9789bfd68e0a04b8d201e717
Adarsha Hadli <adarshhadli96@gmail.com>
2025-10-06 01:58:43 +0530

    component, service, theme folders

---
+++ frontend/src/app/components/audio-chat/audio-chat.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-audio-chat',
+  standalone: true,
+  imports: [],
+  templateUrl: './audio-chat.component.html',
+  styleUrl: './audio-chat.component.scss'
+})
+export class AudioChatComponent {
+
+}
---
+++ frontend/src/app/components/summary-panel/summary-panel.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-summary-panel',
+  standalone: true,
+  imports: [],
+  templateUrl: './summary-panel.component.html',
+  styleUrl: './summary-panel.component.scss'
+})
+export class SummaryPanelComponent {
+
+}
